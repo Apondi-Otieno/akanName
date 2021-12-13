d227599110abf5599bf8872fe8511432081bc368
Apondi Otieno <apondotii@gmail.com>
2021-12-13 14:33:23 +0300

    this will add changes to my js file

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var CC, YY, MM, DD, d, dayValue;
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var female = ["Akosua", "Adwoa", "Abenaa", "Akua", " Yaa", "Afua", "Ama"];
-
+var gender = ["male", "female"];
 
 function validateForm() {
     var genders = document.getElementsByName("gender");
